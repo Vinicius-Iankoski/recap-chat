d7aef704091c3b2ebe9bb0f165361009ef5da1dd
Vinicius Iankoski <vine.iankoski@gmail.com>
2025-10-16 16:39:41 -0300

    pequenos ajustes e testes de funcionamento

--- src/app.js
+++ src/app.js
@@ -1,1 +1,14 @@
-import express from "express";
+import express from 'express';
+import { createServer } from 'node:http';
+
+const app = express();
+const server = createServer(app);
+
+// exemplo de rota
+app.get('/testes', (req, res) => {
+    res.send('<h1>Hello World</h1>')
+});
+
+server.listen(3000, () => {
+    console.log('server esta rodando na porta 3000')
+});
